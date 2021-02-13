a1cbd6fc94d75c5c70a8b1097a62397c872940e0
Chander Shekhar <be.like.chandu@gmail.com>
2021-02-13 23:02:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/newsier/ui/bbc/HomeFragment.java
@@ -0,0 +1,44 @@
+package com.example.newsier.ui.bbc;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+
+import com.example.newsier.R;
+import com.example.newsier.WebViewController;
+
+public class HomeFragment extends Fragment {
+
+    private HomeViewModel homeViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
+
+        WebView webView = root.findViewById(R.id.webViewTimesOfIndia);
+        webView.loadUrl("https://timesofindia.indiatimes.com/");
+        webView.setWebViewClient(new WebViewController());
+
+        Button btn = root.findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_nav_home_to_nav_gallery);
+            }
+        });
+        return root;
+    }
+}
---
+++ app/src/main/java/com/example/newsier/ui/gallery/GalleryViewModel.java
@@ -0,0 +1,19 @@
+package com.example.newsier.ui.gallery;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class GalleryViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public GalleryViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is India Today News fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
